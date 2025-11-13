01661df02b352e738a7b8f554b6617bcb03b1872
Tung Tran <tung.tran@agest.vn>
2025-11-13 17:07:07 +0700

    fixed empty path redirects to /home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ScenarioPage } from './components/scenario-page/scenario-page';
 import { HomePage } from './components/home-page/home-page';
 
 export const routes: Routes = [
-  { path: 'home', component:HomePage },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
   { path: 'scenario/:framework/:component/:control/:sample', component: ScenarioPage },
   // same as the default English path, but with the /ja prefix
   {
     path: 'ja',
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomePage },
       { path: 'scenario/:framework/:component/:control/:sample', component: ScenarioPage },
     ],
